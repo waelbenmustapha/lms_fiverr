e9a3866c72847ccbd7e91742217815c7a6974b83
waelbenmustapha <[Your>
2022-09-17 02:36:32 +0100

    wael done done

--- src/components/coursePage/DataElement.js
+++ src/components/coursePage/DataElement.js
@@ -3,8 +3,9 @@ import { ReactComponent as Arrow } from "../../assets/svg/arrow.svg";
 import { ReactComponent as Lock } from "../../assets/svg/lock.svg";
 import { ReactComponent as Book } from "../../assets/svg/bookOpen.svg";
 import { ReactComponent as Play } from "../../assets/svg/play-circle.svg";
-function DataElement({ element, selectedData, setSelectedData }) {
-  const [collapse, setCollapse] = useState(false);
+function DataElement({ element, selectedData, setSelectedData,collapsed }) {
+  const [collapse, setCollapse] = useState(collapsed);
+  
   console.log(element);
   return (
     <div>
@@ -31,14 +32,33 @@ function DataElement({ element, selectedData, setSelectedData }) {
         </p>
         <div style={{ display: "flex", gap: "12px", alignItems: "center" }}>
           {element.locked && <Lock style={{ opacity: 0.2 }} />}
-          <Arrow style={{ rotate: !collapse ? "180deg" : "0deg" }} />
+          {element.progress!==0 && (
+            <div className="smpercent">
+              <svg>
+                <circle cx="16.8" cy="16.8" r="14"></circle>
+                <circle
+                  cx="16.8"
+                  cy="16.8"
+                  r="14"
+                  style={{ "--percent": element.progress }}
+                ></circle>
+              </svg>
+              <div className="smnumber">
+                <h3>
+                  {element.progress}
+                  <span>%</span>
+                </h3>
+              </div>
+            </div>
+          )}
+          <Arrow style={{ rotate: collapse ? "180deg" : "0deg" }} />
         </div>
       </div>
       {collapse && (
         <div className="datatypes">
           {element.content.map((el) => (
             <div
-              onClick={() => setSelectedData({...el})}
+              onClick={() => setSelectedData({ ...el })}
               className={
                 "dataElement hover-cursor " +
                 (el.finished ? "isElementFinished " : " ") +
