ae427bbb29eb9a11e93ed87e09a8affca7716c51
Saber Mohamed <sabermed66@gmail.com>
2022-09-18 14:04:18 +0100

    callusBox responsive

--- src/components/callUsBox/CallUsBox.js
+++ src/components/callUsBox/CallUsBox.js
@@ -4,8 +4,8 @@ import "./callUsBox.css";
 function CallUsBox({ title, description }) {
   return (
     <div className="call-us">
-      <p className="title mb-20">{title}</p>
-      <p className="description mb-20">{description}</p>
+      <p className="title">{title}</p>
+      <p className="description">{description}</p>
       <button className="btn-call-us">تواصل معنا</button>
     </div>
   );
