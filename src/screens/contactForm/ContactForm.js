cf6b39c367b26c81311f1b0985f871c41fe8478f
Saber Mohamed <sabermed66@gmail.com>
2022-09-19 15:30:40 +0100

    contact form design

--- src/screens/contactForm/ContactForm.js
+++ src/screens/contactForm/ContactForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Formik } from "formik";
 function ContactForm() {
   return (
-    <div className="mx-auto max-w-[850px]  mediamax-1079:pt-[40px] mediamax-860:pt-[20px] pt-[60px] mb-[100px]">
+    <div className="mx-auto max-w-[850px] px-[20px] pt-[60px] mb-[100px]">
       <p className="max-w-[550px] text-[40px] mediamax-1023:text-[28px] mediamax-860:text-[24px] font-bold mb-[40px]">
         لو عندك أسئلة تواصل معنا وراح نجاوبك بأسرع وقت..
       </p>
@@ -31,10 +31,10 @@ function ContactForm() {
           /* and other goodies */
         }) => (
           <form>
-            <div className="w-fit mediamax-650:w-[80%]">
+            <div className="w-full">
               <div className="flex flex-row mediamax-650:flex-col mediamax-650:gap-[15px] gap-[50px] mb-[20px]">
-                <div>
-                  <p className="text-[20px] text-primary-color mb-[8px]">
+                <div className="flex-[1]">
+                  <p className="text-[24px] text-primary-color mb-[8px]">
                     الاسم
                   </p>
                   <input
@@ -46,8 +46,8 @@ function ContactForm() {
                     value={values.name}
                   />
                 </div>
-                <div>
-                  <p className="text-[20px] text-primary-color mb-[8px]">
+                <div className="flex-[1]">
+                  <p className="text-[24px] text-primary-color mb-[8px]">
                     البريد الالكتروني
                   </p>
                   <input
@@ -61,8 +61,8 @@ function ContactForm() {
                 </div>
               </div>
               <div className="flex flex-row mediamax-650:flex-col mediamax-650:gap-[15px] gap-[50px] mb-[20px]">
-                <div>
-                  <p className="text-[20px] text-primary-color mb-[8px]">
+                <div className="flex-[1]">
+                  <p className="text-[24px] text-primary-color mb-[8px]">
                     عنوان الرسالة
                   </p>
                   <input
@@ -74,9 +74,9 @@ function ContactForm() {
                     value={values.title}
                   />
                 </div>
-                <div>
-                  <p className="text-[20px] text-primary-color mb-[8px]">
-                    اسم البرنامج{" "}
+                <div className="flex-[1]">
+                  <p className="text-[24px] text-primary-color mb-[8px]">
+                    اسم البرنامج
                   </p>
                   <input
                     className="input-formit"
@@ -89,7 +89,7 @@ function ContactForm() {
                 </div>
               </div>
               <div className=" w-full flex flex-col ">
-                <p className="text-[20px] text-primary-color mb-[8px]">
+                <p className="text-[24px] text-primary-color mb-[8px]">
                   الرسالة
                 </p>
                 <textarea
@@ -102,7 +102,7 @@ function ContactForm() {
                 />
                 <div
                   onClick={() => handleSubmit()}
-                  className="bg-[#00EC8B] self-end h-[50px] w-[280px] flex justify-center items-center"
+                  className="bg-[#00EC8B] self-end h-[50px] w-full max-w-[280px] mediamax-550:max-w-full flex justify-center items-center"
                 >
                   <p className="text-[20px] font-[bold] text-primary-color">
                     submit
