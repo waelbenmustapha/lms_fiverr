7ecb76f330cfa87ecdef8a1935e636c7f467bb06
waelbenmustapha <[Your>
2022-09-20 16:45:09 +0100

    done

--- src/utils/apis/Auth.js
+++ src/utils/apis/Auth.js
@@ -8,3 +8,23 @@ export const signin = ( data ) => {
       data,
     })
   };
+
+  export const forgetPass = ( data ) => {
+    return execRequest({
+      baseURL: "https://reqres.in/",
+      url: 'api/register',
+      method: 'POST',
+      data,
+    })
+  };
+
+  export const setNewPass = ( data ) => {
+    return execRequest({
+      baseURL: "https://reqres.in/",
+      url: 'api/register',
+      method: 'POST',
+      data,
+    })
+  };
+
+
