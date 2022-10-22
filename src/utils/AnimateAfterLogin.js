3368d42994a924a639e99e31f328e811e9438fa4
waelbenmustapha <[Your>
2022-10-22 21:53:41 +0100

    animate all screens

--- src/utils/AnimateAfterLogin.js
+++ src/utils/AnimateAfterLogin.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { useLocation } from "react-router-dom";
 
 function AnimateAfterLogin({ children }) {
-  const location = useLocation();
-  return location?.state?.from === "login" ? (
+  return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 1 } }}
     >
       {children}
     </motion.div>
-  ) : (
-    <>{children}</>
   );
 }
 
